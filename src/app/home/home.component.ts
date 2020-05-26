2ebb4cb6286e4338f1f6832bc4552564c1d59d4b
Eddie S <esanchez@ezfacility.com>
2020-05-26 16:23:31 -0400

    Should work Everywhere

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,32 +10,25 @@ export class HomeComponent implements OnInit {
 
   zoomUrl = 'https://zoom.us/j/99469935741?pwd=c2R2R01NQmNMcU5OeW1mOUpuempYZz09';
   urlSafe: SafeResourceUrl;
-  output1 = '';
-  output2 = '';
+  openNewWindow = false;
 
   constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    let iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
-    alert('iOS = ' + iOS);
-    this.output1 = 'Platform:' + navigator.platform;
-
-    iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-    alert('iOS = ' + iOS);
-    this.output2 = 'Platform:' + navigator.userAgent + '<br/> MSStream' + window.MSStream;
-
-    if (navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2) {
-      alert('IPAD');
+    const iOSPlatform = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+    const iOSUserAgent = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    const iPad = navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2;
+    if (iOSPlatform || iOSUserAgent || iPad) {
+      this.openNewWindow = true;
     }
-
   }
 
   launchZoom() {
-    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.zoomUrl);
-  }
-
-  tabZoom() {
-    window.open(this.zoomUrl, '_blank');
+    if (this.openNewWindow){
+      window.open(this.zoomUrl, '_blank');
+    } else {
+      this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.zoomUrl);
+    }
   }
 
 }
